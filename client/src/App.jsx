e874c19adc22e13c7cc89e06b7d2a0909ebff1ba
esplanadeA <anna.li.dev@gmail.com>
2023-06-10 14:47:33 +1200

    register page form in place

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import {Route, Routes} from 'react-router-dom';
 import IndexPage from './components/IndexPage';
 import LoginPage from './components/LoginPage';
 import Layout from './Layout';
+import {RegisterPage} from './components/RegisterPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
