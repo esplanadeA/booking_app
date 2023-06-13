8042146ee5df3b49d60842eff5961c468f6f32ef
esplanadeA <anna.li.dev@gmail.com>
2023-06-13 14:46:22 +1200

    register auth page working;

--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -8,13 +8,18 @@ export const RegisterPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function registerUser(event) {
+  async function registerUser(event) {
     event.preventDefault();
-    axios.post('/register', {
-      name,
-      email,
-      password,
-    });
+    try {
+      await axios.post('/register', {
+        name,
+        email,
+        password,
+      });
+      alert('Registration successful.Now you can Log in');
+    } catch (e) {
+      alert('Registration failed. Please try again later');
+    }
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
