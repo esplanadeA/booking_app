06e07fcf0f33cd39945df8f17d5c95191c274d31
esplanadeA <anna.li.dev@gmail.com>
2023-06-13 14:35:17 +1200

    mongooseDB hash password

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 var cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+const bcrypt = require('bcrypt');
+
+const User = require('./models/User');
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+// .ENV
+require('dotenv').config();
 
 var corsOptions = {credential: true, origin: 'http://localhost:5173'};
 
@@ -11,11 +20,26 @@ app.get('/test', (req, res) => {
   res.json('test ok');
 });
 
-app.post('/register', (req, res) => {
+// mongoose
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post('/register', async (req, res) => {
   const {name, email, password} = req.body;
-  console.log('Received data:', name, email, password);
-  res.json({name, email, password});
-  res.status(201).json({message: 'User created successfully'});
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    // Handle any errors that occur during user creation
+    console.error(error);
+    res.status(500).json({message: 'Error creating user'});
+
+    //   res.status(201).json({message: 'User created successfully'});
+  }
 });
 
 app.listen(4000);
