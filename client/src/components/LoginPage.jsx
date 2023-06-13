3d116b519a9778355aec0aeac73047266c0118a2
esplanadeA <anna.li.dev@gmail.com>
2023-06-13 15:57:13 +1200

    login page function added

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLoginSummit(event) {
+    event.preventDefault();
+    try {
+      await axios.post('/login', {
+        email,
+        password,
+      });
+    } catch (e) {
+      alert('Login failed. Please try again later');
+    }
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={{ handleLoginSummit }}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
             Don't have an account yet?
