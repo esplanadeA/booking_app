68903bf70a4d42d73a02839d2c61a0bc72643ef1
esplanadeA <anna.li.dev@gmail.com>
2023-06-15 11:34:53 +1200

    log-in page fixed with redirect feature working

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import {useState} from 'react';
 import {Link, Navigate} from 'react-router-dom';
 import axios from 'axios';
+import {UserContext} from '../UserContext';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState('false');
+  const {setUser} = useContext(UserContext);
 
   async function handleLoginSummit(event) {
     event.preventDefault();
     try {
-      await axios.post('/login', {
+      const userInfo = await axios.post('/login', {
         email,
         password,
       });
+      setUser(userInfo);
       alert('Login successful');
+      setRedirect(true);
     } catch (e) {
       alert('Login failed. Please try again later');
     }
